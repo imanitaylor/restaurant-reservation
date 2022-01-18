ba6452a9fcab6a13ab25a96244d75cb508819dc8
Imani Taylor <imanir.taylor@gmail.com>
2022-01-18 10:26:12 -0600

    first commit

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,6 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
+//to look at the localhost OR our API during deployment
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
