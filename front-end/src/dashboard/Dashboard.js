eb927727c461d8f82b5c6346abc7198e56322138
Imani Taylor <imanir.taylor@gmail.com>
2022-01-21 12:50:34 -0600

    added prev, today, next day buttons

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,15 +1,29 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { listReservations } from "../utils/api";
+import useQuery from "../utils/useQuery";
+import { previous, next, today, formatAsTime } from "../utils/date-time";
 import ErrorAlert from "../layout/ErrorAlert";
-import "./Dashboard.css"
 
+import "./Dashboard.css"
 /**
  * Defines the dashboard page.
  * @param date
  *  the date for which the user wants to view reservations.
  * @returns {JSX.Element}
  */
-function Dashboard({ date }) {  
+function Dashboard({ date }) {
+  const history = useHistory();
+  
+  const query = useQuery();
+  const getDate = query.get("date");
+
+  if (getDate) {
+    date = getDate;
+  } else {
+    date = today();
+  }
+  
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
 
@@ -31,7 +45,7 @@ function Dashboard({ date }) {
             <td>{first_name}</td>
             <td>{last_name}</td>
             <td>{mobile_number}</td>
-            <td>{reservation_time}</td>
+            <td>{formatAsTime(reservation_time)}</td>
             <td>{reservation_date}</td>
             <td>{people}</td>
         </tr>
@@ -63,10 +77,15 @@ function Dashboard({ date }) {
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+        <h4 className="mb-0">Reservations for {date}</h4>
       </div>
       <ErrorAlert error={reservationsError} />
       <div className="row">{currentReservations}</div>
+      <div className="row">
+      <button type="submit" className="btn btn-primary m-2" onClick={() => history.push(`/dashboard?date=${previous(date)}`)}>Prev</button>
+      <button type="submit" className="btn btn-primary m-2" onClick={() => history.push(`/dashboard`)}>Today</button>
+      <button type="submit" className="btn btn-primary m-2" onClick={() => history.push(`/dashboard?date=${next(date)}`)}>Next</button>
+      </div>
     </main>
   );
 }
