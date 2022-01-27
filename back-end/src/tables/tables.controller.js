111dca22261b9a1e53212bee74acd44007212040
Imani Taylor <imanir.taylor@gmail.com>
2022-01-27 13:05:45 -0600

    POST method, creates new table with validation funcs

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -6,6 +6,63 @@
  
  //---Middleware functions---//
 
+ //checks to see if the user input anything into the fields (request body)
+ function validForm(req, res, next) {
+    const { data } = req.body;
+  
+    if (!data) {
+      return next({
+        status: 400,
+        message: `You must include a table name and capacity.`,
+      });
+    }
+    next();
+  }
+  
+  //checks to see if the table includes the required fields to make a table 
+  //table name and table capacity is required
+  function hasRequiredFields(req, res, next) {
+    const { data } = req.body;
+    const requiredFields = [
+      "table_name",
+      "capacity",
+    ];
+    for (const field of requiredFields) {
+      if (!data[field]) {
+        return next({
+          status: 400,
+          message: `Table must include a ${field}`,
+        });
+      }
+    }
+    next();
+  }
+
+//checks to make sure that the capacity is a number and not 0
+function isValidCapacity(req, res, next) {
+    const { data } = req.body;
+    if (!Number.isInteger(data.capacity) || data.capacity === 0) {
+      return next({
+        status: 400,
+        message: "capacity must be a number and higher than 0.",
+      });
+    }
+    next();
+  }
+  
+//checks to make sure the table name has more than 1 character
+  function isValidTableName(req, res, next) {
+    const { data } = req.body;
+
+    if ((data.table_name).length <= 1) {
+      return next({
+        status: 400,
+        message: "table_name must be more than 1 character",
+      });
+    }
+    next();
+  }
+
   //---Router functions---//
 
   async function list(req, res) {
@@ -14,6 +71,20 @@
 
   }
 
+
+  async function create(req, res) {
+    const createdTable = await service.create(req.body.data);
+    res.status(201).json({ data: createdTable });
+  }
+
+
   module.exports = {
     list: asyncErrorBoundary(list),
+    create:[
+        validForm,
+        hasRequiredFields,
+        isValidCapacity,
+        isValidTableName,
+        asyncErrorBoundary(create),
+    ]
   }
--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,10 +1,24 @@
 const knex = require("../db/connection");
 
-//GET, read all tables
+//GET, read all tables, in order by table name
 function list() {
     return knex("tables").select("*").orderBy("table_name");
   }
 
+
+//POST, creating a new table
+function create(table) {
+    return knex("tables")
+      .insert(table)
+      .returning("*")
+      .then((newTable) => newTable[0]);
+  }
+
+
+
+
+
 module.exports = {
 list,
+create,
 }
